8cf3431441e8230878a6f2eee9152922f1f2538b
wg1528 <wg9974804@gmail.com>
2024-10-16 09:30:05 -0300

    Create meu_primeiro_teste.sh
    create file1

---
+++ meu_primeiro_teste.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#comentário
+
+echo 'meu primeiro teste script'
+
+print (10 > 4 and 4 < 7) # v and v = v
+print (10 < 7 or 9 > 5) # f or v = v
+
+mkdir /meu_primeiro_script/comandos
+
+echo 'finalizando meu script'
+
